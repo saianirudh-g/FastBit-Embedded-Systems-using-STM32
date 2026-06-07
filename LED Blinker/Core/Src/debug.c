ccf5900aca1f0c946753e201a73ebb0004b781c0
saianirudh-g <133767124+saianirudh-g@users.noreply.github.com>
2026-06-07 04:10:18 -0500

    Create debug.c

---
+++ LED Blinker/Core/Src/debug.c
@@ -0,0 +1,15 @@
+/*
+ * debug.c
+ *
+ *  Created on: 07-Jun-2026
+ *      Author: saian
+ */
+
+#ifdef DEBUG
+#include <stdint.h>
+
+float d_roll_angle = 0.0f;
+uint32_t d_roll_angle_filt = 0;
+float d_dt = 0.0f;
+
+#endif
